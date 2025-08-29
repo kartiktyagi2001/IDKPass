9063568a5bdbcafc2d657dfea3d75e472e76650f
Kartik Tyagi <23mca063@jssaten.ac.in>
2025-08-30 01:26:46 +0530

    added footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,26 @@
 'use client'
 
+import Link from 'next/link';
+
 
 export function Footer(){
     
     return(
-        <div className="bg-[#111826]">
-                        
+        <div className="bg-[#000000] p-4">
+            <div className="flex flex-col items-center gap-3 md:justify-between mx-4">
+                <div>
+                    <img src="/logo.svg" alt="" className="w-10 h-10 rounded-full" />
+                </div>
+                <div className="text-white flex flex-col md:flex-row justify-center items-center gap-3">
+                    <Link href="/about" className="hover:underline cursor-pointer mx-2">About</Link>
+                    <Link href="https://github.com/kartiktyagi2001/IDKPass" className="hover:underline cursor-pointer mx-2">Source Code</Link>
+                    <span className="mx-2">&copy; {new Date().getFullYear()}</span>
+                </div>
+            </div>
+
+            <div className="text-center mt-5 text-white">
+                <p>Crafted by <Link href="https://arcbit.vercel.app/" target='_blank' className="text-[#ff7f4d] hover:underline">Kartik Tyagi</Link></p>
+            </div>     
         </div>
     )
 }
